fd44215321be1a50857a9a8ab233e0e4e1e21409
kebishaa <kbkibatu@gmail.com>
2022-10-24 19:26:40 +0300

    test reverse location

--- tests/test_data_transformation.py
+++ tests/test_data_transformation.py
@@ -13,4 +13,13 @@ class TestCleaner(unittest.TestCase):
         test_df_cols=list(test_df.columns.values)
         new_cols=list(cleaner.remove_space(test_df).columns.values)
         self.assertFalse(test_df_cols == new_cols)
+    def test_remove_col_space(self):
+        test_df=pd.DataFrame(columns=["col one","col_two"])
+        no_space_cols=["col_one","col_two"]
+        new_cols=list(cleaner.remove_space(test_df).columns.values)
+        self.assertEqual(no_space_cols , new_cols)
     
+    def test_reverse_location(self):
+        location = (8.987664535755162, 38.789648739472355)
+        test_df=pd.DataFrame(data=[[8.987664535755162, 38.789648739472355]],columns=["latitude","longitude"])
+        self.assertEqual(cleaner.reverse_location(test_df).location.values[0].strip(),"አዲስ አበባ / Addis Ababa")
